d285eeb958ce0ad2fa38f1fa20b3b4291d63a85d
anurag-asr <anurjwt@gmail.com>
2022-11-09 22:49:03 +0530

    menu and navbar some work done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 
 import './App.css';
-
+import Header from "./Header" 
 function App() {
   return (
     <div className="App">
-     <h1>ModeSens</h1>
+    <Header/>
     </div>
   );
 }
---
+++ frontend/src/Header.jsx
@@ -0,0 +1,149 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <Menu>
+  <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+    Your Cats
+  </MenuButton>
+  <MenuList>
+    <MenuItem minH='48px'>
+      <Image
+        boxSize='2rem'
+        borderRadius='full'
+        src='https://placekitten.com/100/100'
+        alt='Fluffybuns the destroyer'
+        mr='12px'
+      />
+      <span>Fluffybuns the Destroyer</span>
+    </MenuItem>
+    <MenuItem minH='40px'>
+      <Image
+        boxSize='2rem'
+        borderRadius='full'
+        src='https://placekitten.com/120/120'
+        alt='Simon the pensive'
+        mr='12px'
+      />
+      <span>Simon the pensive</span>
+    </MenuItem>
+  </MenuList>
+</Menu>
+
+<Menu>
+  <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+    Your Cats
+  </MenuButton>
+  <MenuList>
+    <MenuItem minH='48px'>
+      <Image
+        boxSize='2rem'
+        borderRadius='full'
+        src='https://placekitten.com/100/100'
+        alt='Fluffybuns the destroyer'
+        mr='12px'
+      />
+      <span>Fluffybuns the Destroyer</span>
+    </MenuItem>
+    <MenuItem minH='40px'>
+      <Image
+        boxSize='2rem'
+        borderRadius='full'
+        src='https://placekitten.com/120/120'
+        alt='Simon the pensive'
+        mr='12px'
+      />
+      <span>Simon the pensive</span>
+    </MenuItem>
+  </MenuList>
+</Menu>
+
+<Menu>
+  <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+    Your Cats
+  </MenuButton>
+  <MenuList>
+    <MenuItem minH='48px'>
+      <Image
+        boxSize='2rem'
+        borderRadius='full'
+        src='https://placekitten.com/100/100'
+        alt='Fluffybuns the destroyer'
+        mr='12px'
+      />
+      <span>Fluffybuns the Destroyer</span>
+    </MenuItem>
+    <MenuItem minH='40px'>
+      <Image
+        boxSize='2rem'
+        borderRadius='full'
+        src='https://placekitten.com/120/120'
+        alt='Simon the pensive'
+        mr='12px'
+      />
+      <span>Simon the pensive</span>
+    </MenuItem>
+  </MenuList>
+</Menu>
+
+<Menu>
+  <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+    Your Cats
+  </MenuButton>
+  <MenuList>
+    <MenuItem minH='48px'>
+      <Image
+        boxSize='2rem'
+        borderRadius='full'
+        src='https://placekitten.com/100/100'
+        alt='Fluffybuns the destroyer'
+        mr='12px'
+      />
+      <span>Fluffybuns the Destroyer</span>
+    </MenuItem>
+    <MenuItem minH='40px'>
+      <Image
+        boxSize='2rem'
+        borderRadius='full'
+        src='https://placekitten.com/120/120'
+        alt='Simon the pensive'
+        mr='12px'
+      />
+      <span>Simon the pensive</span>
+    </MenuItem>
+  </MenuList>
+</Menu>
+
+<Menu>
+  <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+    Your Cats
+  </MenuButton>
+  <MenuList>
+    <MenuItem minH='48px'>
+      <Image
+        boxSize='2rem'
+        borderRadius='full'
+        src='https://placekitten.com/100/100'
+        alt='Fluffybuns the destroyer'
+        mr='12px'
+      />
+      <span>Fluffybuns the Destroyer</span>
+    </MenuItem>
+    <MenuItem minH='40px'>
+      <Image
+        boxSize='2rem'
+        borderRadius='full'
+        src='https://placekitten.com/120/120'
+        alt='Simon the pensive'
+        mr='12px'
+      />
+      <span>Simon the pensive</span>
+    </MenuItem>
+  </MenuList>
+</Menu>
+    </div>
+  )
+}
+
+export default Header
