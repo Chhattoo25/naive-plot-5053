c5623ac4210adddfbc7c26b09f741ed6fc19a57d
Pradeep2888 <pradeeptiwari2888@gmail.com>
2022-11-12 22:55:40 +0530

    seaarch functionality filter functionality created

--- backend/routes/products.route.js
+++ backend/routes/products.route.js
@@ -5,20 +5,47 @@ const dotenv=require("dotenv");
 const { ProductModel } = require("../models/Product.model");
 const { authentication } = require("../middlewares/authentication");
 const { autherisation } = require("../middlewares/autherisation");
+const { UserModel } = require("../models/User.model");
+const { type } = require("os");
 
-productsRouter.get("/",async(req,res)=>{
-    const data=await ProductModel.find()
-    res.send({data:data})
+
+productsRouter.get("/",authentication,autherisation,async(req,res)=>{
+    const {user_id}=req.body
+    const {user_type}=await UserModel.findOne({_id:user_id})
+    if(user_type==="seller"){
+        const data=await ProductModel.find({user_id:user_id})
+        res.send({data})
+    }
+    else if(user_type==="admin"){
+        const data=await ProductModel.find({name:{$regex:name,$options:"i"}}).sort({price:Number(sort)})
+        res.send({data})
+    }
+    else{
+        res.send({"msg":"You are Not authorised user "})
+    }
+})
+productsRouter.get("/filter",async(req,res)=>{
+    const {type,sub_type,category,sort}=req.query
+    const data=await ProductModel.find({$or:[{type:type},{sub_type:sub_type},{category:category}]}).sort({price:Number(sort)})
+    res.send({data})
+  
 })
 
-productsRouter.get("/:user_id",async(req,res)=>{
-    const {user_id}=req.params
-    const data=await ProductModel.findOne({user_id:user_id})
-    res.send({data:data})
+productsRouter.get("/:id",authentication,autherisation,async(req,res)=>{
+           const {id}=req.params
+           console.log(id)
+           const data=await ProductModel.findOne({_id:id})
+           res.send({data})
+})
+
+productsRouter.get("/search",async(req,res)=>{
+           const {name,title}=req.query
+           const data=await ProductModel.find({name:{$regex:name,$options:"i"}}).sort({price:Number(sort)})
+           res.send({data})
 })
 
 productsRouter.post("/addproduct",authentication,autherisation,async(req,res)=>{
-    const {title,type,name,sub_type,category,colour,price,brand,discription,image1,image2,image3,user_id}=req.body
+    const {title,type,name,sub_type,category,colour,price,quantity,brand,discription,image1,image2,image3,user_id}=req.body
     const new_product = new ProductModel({
         title,
         type,
@@ -27,6 +54,7 @@ productsRouter.post("/addproduct",authentication,autherisation,async(req,res)=>{
         category,
         colour,
         price,
+        quantity,
         brand,
         discription,
         image1,
@@ -35,7 +63,6 @@ productsRouter.post("/addproduct",authentication,autherisation,async(req,res)=>{
         user_id:user_id
     })
     await new_product.save()
-    // console.log(new_product)
     res.send({new_product})
 })
 
@@ -49,11 +76,11 @@ productsRouter.delete("/delete/:id",authentication,autherisation,async(req,res)=
     else{
         res.send({"msg":"product not exist more"})   
     }
-    console.log(data)
+
 })
 
 
-productsRouter.patch("/update/:id",authentication,autherisation,async(req,res)=>{
+productsRouter.patch("/update/:id",async(req,res)=>{
            const {id}=req.params
            const body=req.body
           res.send({"msg":body})   
