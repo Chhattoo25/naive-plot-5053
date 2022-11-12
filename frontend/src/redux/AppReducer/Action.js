93f23e4204d1357db74c43aa861682e9e8699c5b
jhaaman1 <ajha9608@gmail.com>
2022-11-12 22:00:41 +0530

    cart section

--- frontend/src/redux/AppReducer/Action.js
+++ frontend/src/redux/AppReducer/Action.js
@@ -12,4 +12,17 @@ export const getData =(params)=> (dispatch)=>{
     dispatch({type:type.GET_DATA_FAILURE,payload:e})
     })
    }
+
+   const getData=()=>{
+    const token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjM2Y2FkYjM3YjRkZGRhNTc4ZjNlZWVhIiwiaWF0IjoxNjY4MTUyMzcwfQ.ioJXgNiBV65ttbJaXleTd2gwou45J1DfOTdWKkeEc4Y"
+      axios.get("http://localhost:8000/product/",{headers:{
+        "Authorization" : `Bearer ${token}`
+      }})
+      .then((res)=>{
+         setData(res.data.data)
+      })
+      .catch((err)=>{
+          console.log(err)
+      })
+  }
    
