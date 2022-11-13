e0a4145551152243efbe7d22adca584390395dd9
Chhattoo25 <95302680+Chhattoo25@users.noreply.github.com>
2022-11-13 11:39:26 +0530

    day-5 done

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -1,17 +1,23 @@
 const { Router } = require("express");
 
-const ProductModel = require("../models/Product.model");
+const {ProductModel} = require("../models/Product.model");
 
 const serachRoute = Router();
 
+
+serachRoute.get("/",async(req,res)=>{
+  const data=await ProductModel.find()
+  res.send({data:data})
+})
 serachRoute.get("/search", async (req, res) => {
   try {
+    const {name,title,price}=req.query
 const page = parseInt(req.query.page)-1 || 0
 const limit = parseInt(req.query.limit)||5
     const search = req.query.search || "";
     let sort = req.query.sort || "";
-    const type = ["Beauty"];
-    type === "All" ? (type = [...type]) : (type = req.query.type.split("&"));
+    // const type = ["Beauty"];
+    // type === "All" ? (type = [...type]) : (type = req.query.type.split(","));
     req.query.sort ? (sort = req.query.sort.split(",")) : (sort = [sort]);
 
     let sortBy = {};
@@ -21,17 +27,17 @@ const limit = parseInt(req.query.limit)||5
       sortBy[(sort[0] = "asc")];
     }
 
-    const data = await ProductModel.find({name:{$regex:search,$options:"i"}}).where("type").in([...type]).sort(sortBy).skip(page*limit).limit(limit)
-const total = await ProductModel.countDocuments({
-type:{$in:[...type]},
-name:{$regex:search,$options:"i"}
-})
+    // const data = await ProductModel.find({name:{$regex:search,$options:"i"}}).sort(sortBy).skip(page*limit).limit(limit)
+    const data=await ProductModel.find({name:{$regex:name,$options:"i"}}).sort({price:Number(sort)})
+    res.send({data})
+// const total = await ProductModel.countDocuments({
+// type:{$in:[...type]},
+// name:{$regex:search,$options:"i"}
+// })
  const response = {
  error:false,
- total,
  page:page+1,
  limit,
- type,
  data
  }
  res.status(200).json(response)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,14 @@
-import AllRoutes from './AllRoutes/AllRoutes';
-
-import './App.css';
-
-import Body from './Body';
-import Footer from './Footer';
-import Header from './Header';
-
+import AllRoutes from "./AllRoutes/AllRoutes";
+import Header from "./Header";
+import Footer from "./Footer";
 function App() {
   return (
     <div className="App">
- <AllRoutes/> 
-
-       </div> 
-
-
-
- );
-}
-
-import Header from './Header';
-function App() {
-  return (
-    <div className="App">
-      <AllRoutes/>
+    {/* <Header/> */}
+      <AllRoutes />
+      {/* <Footer/> */}
     </div>
   );
-
+}
 
 export default App;
