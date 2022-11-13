5d329511c3628a437d1295ee961e5cc65161325e
anurag-asr <anurjwt@gmail.com>
2022-11-13 11:18:52 +0530

    add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,12 @@ import './App.css';
 import Body from './Body';
 import Footer from './Footer';
 import Header from './Header';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
 
 function App() {
   return (
     <div className="App">
-      <h1><FontAwesomeIcon icon={faEnvelope} /></h1>
+
          <AllRoutes/>
          <Header/>
          <Body/>
