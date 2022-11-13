c042f732bfaac425ac360595692ef0adb7ea4482
jhaaman1 <ajha9608@gmail.com>
2022-11-13 12:53:52 +0530

    adding backend url to frontend beauty code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
+
 import AllRoutes from "./AllRoutes/AllRoutes";
-import Header from './Header';
+import './App.css';
+
 function App() {
   return (
     <div className="App">
--- frontend/src/redux/AppReducer/Action.js
+++ frontend/src/redux/AppReducer/Action.js
@@ -3,26 +3,14 @@ import * as type from "./ActionType";
 
 export const getData =(params)=> (dispatch)=>{
     dispatch({type:type.GET_DATA_REQUEST})
-   return axios.get('https://modesens-web-app-backend.herokuapp.com/beauty',{params})
+  //  return axios.get('https://modesens-web-app-backend.herokuapp.com/beauty',{params})
+  return axios.get("http://localhost:8000/product/filter?gender=women&category=beauty",{params})
     .then((r)=>{
-                      
-    dispatch({type:type.GET_DATA_SUCCESS,payload:r.data})
-    })
-    .catch((e)=>{
-    dispatch({type:type.GET_DATA_FAILURE,payload:e})
+         console.log(r.data.data);             
+      dispatch({type:type.GET_DATA_SUCCESS,payload:r.data.data})
+      })
+      .catch((e)=>{
+      dispatch({type:type.GET_DATA_FAILURE,payload:e})
     })
    }
 
-  //  const getData=()=>{
-  //   const token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjM2Y2FkYjM3YjRkZGRhNTc4ZjNlZWVhIiwiaWF0IjoxNjY4MTUyMzcwfQ.ioJXgNiBV65ttbJaXleTd2gwou45J1DfOTdWKkeEc4Y"
-  //     axios.get("http://localhost:8000/product/",{headers:{
-  //       "Authorization" : `Bearer ${token}`
-  //     }})
-  //     .then((res)=>{
-  //        setData(res.data.data)
-  //     })
-  //     .catch((err)=>{
-  //         console.log(err)
-  //     })
-  // }
-   
