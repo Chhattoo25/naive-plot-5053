9f4a99778b4e53154fd7b8207b81a06e7a9dc244
Ppreedam <ppreedam@gmail.com>
2022-11-13 10:37:04 +0530

    Adding

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,14 @@ import './App.css';
 import Login from './UMS/Login';
 import Signup from './UMS/Signup';
 import GoogleToggle from './UMS/googleToggle';
+import AllRoutes from "./AllRoutes/AllRoutes"
 
 
 
 function App() {
   return (
     <div className="App">
-     
+     <AllRoutes/>
     </div>
   );
 }
