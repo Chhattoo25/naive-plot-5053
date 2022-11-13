ee89d4fe8ebbadcd73d0e161bd87fd689e3bea29
jhaaman1 <ajha9608@gmail.com>
2022-11-13 18:45:34 +0530

    adding to cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 
 import AllRoutes from "./AllRoutes/AllRoutes";
-// import Header from './Header';
 import './App.css';
+
 function App() {
   return (
     <div className="App">
-    {/* <Header/> */}
+      {/* <Header/> */}
       <AllRoutes />
       {/* <Footer/> */}
 
